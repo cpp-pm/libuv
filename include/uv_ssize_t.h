5db3276f187a9db1f695037f6de1e7afc7d45ec8
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-08-30 14:19:44 +0300

    Fix OSX build

--- include/uv_ssize_t.h
+++ include/uv_ssize_t.h
@@ -1,13 +1,13 @@
 #ifndef UV_SSIZE_T_H
 #define UV_SSIZE_T_H
 
-#include <stdlib.h> // ssize_t
-
 #if defined(_SSIZE_T_) || defined(_SSIZE_T_DEFINED)
+# include <stdlib.h> // ssize_t
 typedef ssize_t uv_ssize_t;
 #elif defined(_WIN32)
 typedef intptr_t uv_ssize_t;
 #else
+# include <unistd.h> // ssize_t
 typedef ssize_t uv_ssize_t;
 #endif
 
